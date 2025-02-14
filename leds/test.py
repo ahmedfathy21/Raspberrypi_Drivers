cd322db801b75ee08ff63781471b73905716e54e
Ahmed Fathy <150164291+ahmedfathy21@users.noreply.github.com>
2025-02-14 19:41:36 +0200

    Delete leds directory

--- leds/test.py
+++
@@ -1,9 +0,0 @@
-import RPi.GPIO as gpio
-import time
-gpio.setmode(gpio.BCM)  # 15
-gpio.setup(2, gpio.OUT)
-while(True):
-    gpio.output(2, 1)
-    time.sleep(1)
-    gpio.output(2, 0)
-    time.sleep(1)
